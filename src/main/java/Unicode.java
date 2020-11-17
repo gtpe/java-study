d01da568dbbc8ebeae450964422011ed66f214d2
gtpe <gtpezyb@gmail.com>
2020-11-17 11:50:26 +0900

    .

---
+++ src/main/java/Unicode.java
@@ -0,0 +1,15 @@
+public class Unicode {
+
+    public static void main(String[] args) {
+        // \u0000 ~ \uFFFF
+        char a = (char)Integer.parseInt("0048", 16);
+        System.out.println(a);
+
+        char b = '\u0048';
+        System.out.println(b);
+
+        // HELLO
+        System.out.println("\u0048\u0045\u004C\u004C\u004F");
+    }
+
+}
